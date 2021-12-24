ff4f8b4945dd06724d162acbe42356a1924921f0
mariamsaed19 <58952137+mariamsaed19@users.noreply.github.com>
2021-12-24 02:03:01 +0200

    Add files via upload

--- code/unit_testing/auth_test.py
+++ code/unit_testing/auth_test.py
@@ -1,10 +1,28 @@
 import unittest
 
+from auth_proxy.sign_in import SignIn
+from auth_proxy.sign_up import SignUp
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_sign_up(self):
+        s = SignUp()
+        s.sign_up("Admin2", "12345678", False)
+        self.assertEqual(s.sign_up("new", "123", False), (False, "Invalid password!"))
+        self.assertEqual(s.sign_up("Admin2", "12345678", False), (False, "Name already exists!"))
+        check, player=s.sign_up("name1", "pass12345", False)
+        self.assertEqual(player.get_name(), "name1")
+        self.assertTrue(check)
 
+    def test_sign_in(self):
+        s = SignIn()
+        self.assertEqual(s.sign_in("new", "123"), (False, "Player doesn't exist!"))
+        s2 = SignUp()
+        s2.sign_up("Admin3", "12345678", False)
+        self.assertEqual(s.sign_in("Admin3", "123456"), (False, "Wrong password!"))
+        check,player=s.sign_in("Admin3", "12345678")
+        self.assertTrue(check)
+        self.assertEqual(player.get_name(), "Admin3")
 
 if __name__ == '__main__':
     unittest.main()
