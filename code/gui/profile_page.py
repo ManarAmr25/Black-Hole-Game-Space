d1f8eb96d2b828f3a6f961269d0c324c25fb80a5
ManarAmr25 <59005827+ManarAmr25@users.noreply.github.com>
2021-12-28 03:20:00 +0200

    small fixes in profile

--- code/gui/profile_page.py
+++ code/gui/profile_page.py
@@ -1,10 +1,25 @@
 from PyQt5 import QtCore, QtWidgets
 from PyQt5 import QtGui
 from PyQt5.QtGui import QMovie, QPainter
-from PyQt5.QtWidgets import  QMainWindow,QWidget
+from PyQt5.QtWidgets import QMainWindow, QWidget
+
+from auth_proxy.facade import Facade
 
 
 class ProfileWindow(QWidget):
+
+    def toggle_field(self):
+        if self.lineEdit.isEnabled():  # button is active now and we need to save changes
+            self.pushButton_2.setText("Edit")
+            print(self.lineEdit.text())
+            f = Facade()  # TODO : facade needs to have more functionality from data base manager
+            self.lineEdit.setDisabled(True)
+        else:  # button is disabled and we need to make changes
+            print("edit name")
+            self.pushButton_2.setText("Save")
+            self.lineEdit.setEnabled(True)
+            self.lineEdit.setFocus(True)
+
     def __init__(self, parent=None):
         super(ProfileWindow, self).__init__(parent)
         self.setObjectName("Form")
@@ -17,7 +32,7 @@ class ProfileWindow(QWidget):
         icon.addPixmap(QtGui.QPixmap("icons8-space-64.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(QtCore.QRect(380, 40, 631, 81))
+        self.label.setGeometry(QtCore.QRect(700, 40, 631, 81))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(36)
@@ -28,7 +43,7 @@ class ProfileWindow(QWidget):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self)
-        self.label_2.setGeometry(QtCore.QRect(30, 220, 251, 261))
+        self.label_2.setGeometry(QtCore.QRect(90, 220, 251, 261))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -49,7 +64,7 @@ class ProfileWindow(QWidget):
         self.label_4.setScaledContents(True)
         self.label_4.setObjectName("label_4")
         self.label_5 = QtWidgets.QLabel(self)
-        self.label_5.setGeometry(QtCore.QRect(340, 250, 251, 41))
+        self.label_5.setGeometry(QtCore.QRect(450, 250, 251, 41))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(22)
@@ -61,7 +76,7 @@ class ProfileWindow(QWidget):
         self.label_5.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         self.label_5.setObjectName("label_5")
         self.pushButton = QtWidgets.QPushButton(self)
-        self.pushButton.setGeometry(QtCore.QRect(10, 510, 291, 41))
+        self.pushButton.setGeometry(QtCore.QRect(70, 510, 291, 41))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(11)
@@ -90,7 +105,7 @@ class ProfileWindow(QWidget):
         self.pushButton.setFlat(False)
         self.pushButton.setObjectName("pushButton")
         self.label_7 = QtWidgets.QLabel(self)
-        self.label_7.setGeometry(QtCore.QRect(340, 410, 201, 41))
+        self.label_7.setGeometry(QtCore.QRect(450, 410, 201, 41))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(22)
@@ -102,7 +117,7 @@ class ProfileWindow(QWidget):
         self.label_7.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         self.label_7.setObjectName("label_7")
         self.lineEdit = QtWidgets.QLineEdit(self)
-        self.lineEdit.setGeometry(QtCore.QRect(620, 240, 341, 61))
+        self.lineEdit.setGeometry(QtCore.QRect(730, 240, 341, 61))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(20)
@@ -127,7 +142,7 @@ class ProfileWindow(QWidget):
         self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton_2 = QtWidgets.QPushButton(self)
-        self.pushButton_2.setGeometry(QtCore.QRect(1020, 250, 71, 41))
+        self.pushButton_2.setGeometry(QtCore.QRect(1130, 250, 71, 41))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(11)
@@ -153,7 +168,7 @@ class ProfileWindow(QWidget):
                                         "}")
         self.pushButton_2.setObjectName("pushButton_2")
         self.label_6 = QtWidgets.QLabel(self)
-        self.label_6.setGeometry(QtCore.QRect(1130, 250, 260, 31))
+        self.label_6.setGeometry(QtCore.QRect(1240, 250, 260, 31))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -165,7 +180,7 @@ class ProfileWindow(QWidget):
         self.label_6.setAlignment(QtCore.Qt.AlignCenter)
         self.label_6.setObjectName("label_6")
         self.pushButton_3 = QtWidgets.QPushButton(self)
-        self.pushButton_3.setGeometry(QtCore.QRect(450, 340, 191, 41))
+        self.pushButton_3.setGeometry(QtCore.QRect(560, 340, 191, 41))
         self.pushButton_3.setStyleSheet("QPushButton{\n"
                                         "    color:rgb(248, 255, 105);\n"
                                         "    font: 11pt \"OCR A Extended\";\n"
@@ -184,14 +199,15 @@ class ProfileWindow(QWidget):
                                         "}")
         self.pushButton_3.setObjectName("pushButton_3")
         self.frame = QtWidgets.QFrame(self)
-        self.frame.setGeometry(QtCore.QRect(440, 480, 721, 341))
+        self.frame.setGeometry(QtCore.QRect(550, 480, 721, 341))
         self.frame.setStyleSheet("border-left : 7px solid rgb(255, 255, 255);\n"
                                  "background-color : rgba(184, 228, 240,0.3); ")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
         self.label_8 = QtWidgets.QLabel(self.frame)
-        self.label_8.setGeometry(QtCore.QRect(20, 20, 191, 31))
+        self.label_8.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.label_8.setGeometry(QtCore.QRect(20, 20, 230, 35))
         font = QtGui.QFont()
         font.setFamily("OCR A Extended")
         font.setPointSize(11)
@@ -199,8 +215,35 @@ class ProfileWindow(QWidget):
         self.label_8.setStyleSheet("color: rgb(255, 255, 255);\n"
                                    "border : none;")
         self.label_8.setObjectName("label_8")
+        # TODO :
+        self.label_9 = QtWidgets.QLabel(self.frame)
+        self.label_9.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.label_9.setGeometry(QtCore.QRect(20, 90, 230, 35))
+        self.label_9.setFont(font)
+        self.label_9.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                   "border : none;")
+        self.label_9.setObjectName("label_9")
+
+        # TODO :
+        self.label_10 = QtWidgets.QLabel(self.frame)
+        self.label_10.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.label_10.setGeometry(QtCore.QRect(20, 160, 230, 35))
+        self.label_10.setFont(font)
+        self.label_10.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                   "border : none;")
+        self.label_10.setObjectName("label_10")
+
+        # TODO :
+        self.label_11 = QtWidgets.QLabel(self.frame)
+        self.label_11.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.label_11.setGeometry(QtCore.QRect(20, 230, 230, 35))
+        self.label_11.setFont(font)
+        self.label_11.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                   "border : none;")
+        self.label_11.setObjectName("label_11")
+
         self.pushButton_4 = QtWidgets.QPushButton(self)
-        self.pushButton_4.setGeometry(QtCore.QRect(30, 910, 80, 80))
+        self.pushButton_4.setGeometry(QtCore.QRect(40, 870, 80, 80))
         self.pushButton_4.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.pushButton_4.setStyleSheet("QPushButton{\n"
                                         "    border-radius : 40px;\n"
@@ -243,10 +286,17 @@ class ProfileWindow(QWidget):
         self.pushButton.setText(_translate("Form", "Change Profile Picture"))
         self.label_7.setText(_translate("Form", "Stats:"))
         self.lineEdit.setText(_translate("Form", "Player\'s name"))
+        self.lineEdit.setDisabled(True)
         self.pushButton_2.setText(_translate("Form", "Edit"))
+        # TODO : is this the right place for this line of code
+        self.pushButton_2.clicked.connect(self.toggle_field)
         self.label_6.setText(_translate("Form", "User Name already exists"))
+        self.label_6.setVisible(False)
         self.pushButton_3.setText(_translate("Form", "Change password"))
-        self.label_8.setText(_translate("Form", " number of wins : "))
+        self.label_8.setText(_translate("Form", "  number of wins : "))
+        self.label_9.setText(_translate("Form", "  number of games : "))
+        self.label_10.setText(_translate("Form", "  daily challenges : "))
+        self.label_11.setText(_translate("Form", "  this week's xp : "))
 
 
 class ProfileMain(QMainWindow):
@@ -270,5 +320,3 @@ class ProfileMain(QMainWindow):
         if frameRect.intersects(event.rect()):
             painter = QPainter(self)
             painter.drawPixmap(frameRect.left(), frameRect.top(), currentFrame)
-
-
